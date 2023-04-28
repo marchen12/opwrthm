1c5f4344a4d014babca2b114b4a3b6ee33142bcc
marchen12 <130596644+marchen12@users.noreply.github.com>
2023-04-28 18:42:36 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -2,11 +2,11 @@
 #=================================================
 # Description: DIY script
 # Lisence: MIT
-# Author: eSirPlayground
-# Youtube Channel: https://goo.gl/fvkdwm 
+# Author: CCHEN
+# Youtube Channel:8888
 #=================================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.6.1/g' openwrt/package/base-files/files/bin/config_generate
 
 #2. Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
